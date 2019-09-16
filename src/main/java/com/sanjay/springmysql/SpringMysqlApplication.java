d7ea39d4de561efbf0f59b0c2f5bb3a8ed6da784
sanjay manna <sanjaysm91@gmail.com>
2019-09-16 13:56:43 +0530

    configuration added

--- src/main/java/com/sanjay/springmysql/SpringMysqlApplication.java
+++ src/main/java/com/sanjay/springmysql/SpringMysqlApplication.java
@@ -1,19 +1,19 @@
 package com.sanjay.springmysql;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories(basePackages = "com.sanjay.springmysql.repository")
 @SpringBootApplication
 public class SpringMysqlApplication extends SpringBootServletInitializer {
-
+	/*Deployment for server*/
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringMysqlApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMysqlApplication.class, args);
 	}
